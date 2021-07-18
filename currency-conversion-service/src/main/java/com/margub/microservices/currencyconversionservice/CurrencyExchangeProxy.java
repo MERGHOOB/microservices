9640455848396e93f3b11896ae1860191fc265e2
mekhan <margub.khan@gmail.com>
2021-07-18 12:47:11 +0530

    # to avoid Load balancer does not have available server for client eureka.client.service-url.default-zone=http://localhost:8761/eureka eureka.instance.prefer-ip-address=true
    spring.cloud.gateway.discovery.locator.enabled=true
    spring.cloud.gateway.discovery.locator.lower-case-service-id=true //for lowercase

--- currency-conversion-service/src/main/java/com/margub/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/margub/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000/")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000/") // comment out to use load balancing done by eureka
+@FeignClient(name = "currency-exchange")  //remove url to use eureka load balancing
 public interface CurrencyExchangeProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
